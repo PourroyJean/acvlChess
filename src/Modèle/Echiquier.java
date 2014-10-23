2429537652ab9541096b57caad87a293c62bcf19
Paul Tardy <paul.tardy@live.fr>
2014-10-23 10:32:02 +0200

    Changement de la modelisation de l'échiquier

---
+++ src/Modèle/Echiquier.java
@@ -0,0 +1,16 @@
+package Modèle;
+
+/**
+ * Created by Paul on 20/10/2014.
+ */
+public class Echiquier {
+    private Piece[][] tableau;
+
+    public Echiquier() {
+
+    }
+
+    public Echiquier(Piece[][] tableau) {
+        this.tableau = tableau;
+    }
+}
--- src/Modèle/Jeu.java
+++ src/Modèle/Jeu.java
@@ -6,7 +6,7 @@ package Modèle;
 
 // Singleton jeu
 public class Jeu {
-    private Piece[][] echiquier;
+    private Echiquier echiquier;
     private Joueur blanc;
     private Joueur noir;
     final private static Jeu jeu = new Jeu();
@@ -14,39 +14,45 @@ public class Jeu {
     private Jeu() {
         blanc   = new Joueur();
         noir    = new Joueur();
-        echiquier = new Piece[8][8];
 
+        Piece[][] tableau = new Piece[8][8];
         // BLANCS - Reine/Roi
-        echiquier[3][0] =  new Reine	(blanc, new Coordonnee(3,0));
-        echiquier[4][0] =  new Roi	(blanc, new Coordonnee(4,0));
-            //fous
-        echiquier[5][0] =  new Fou(blanc, new Coordonnee(5,0));
-        echiquier[2][0] =  new Fou(blanc, new Coordonnee(2,0));
-            //cavaliers
-        echiquier[6][0] =  new Cavalier(blanc, new Coordonnee(6,0));
-        echiquier[1][0] =  new Cavalier(blanc, new Coordonnee(1,0));
-            //tours
-        echiquier[0][0] =  new Tour	(blanc, new Coordonnee(0,0));
-        echiquier[7][0] =  new Tour	(blanc, new Coordonnee(7,0));
+        tableau[3][0] =  new Reine	(blanc, new Coordonnee(3,0));
+        tableau[4][0] =  new Roi	(blanc, new Coordonnee(4,0));
+        //fous
+        tableau[5][0] =  new Fou(blanc, new Coordonnee(5,0));
+        tableau[2][0] =  new Fou(blanc, new Coordonnee(2,0));
+        //cavaliers
+        tableau[6][0] =  new Cavalier(blanc, new Coordonnee(6,0));
+        tableau[1][0] =  new Cavalier(blanc, new Coordonnee(1,0));
+        //tours
+        tableau[0][0] =  new Tour	(blanc, new Coordonnee(0,0));
+        tableau[7][0] =  new Tour	(blanc, new Coordonnee(7,0));
 
         // NOIRS - Reine/Roi
-        echiquier[3][7] =  new Reine	(noir, new Coordonnee(3,0));
-        echiquier[4][7] =  new Roi	(noir, new Coordonnee(4,0));
-            //fous
-        echiquier[5][7] =  new Fou(noir, new Coordonnee(5,0));
-        echiquier[2][7] =  new Fou(noir, new Coordonnee(2,0));
-            //cavaliers
-        echiquier[6][7] =  new Cavalier(noir, new Coordonnee(6,0));
-        echiquier[1][7] =  new Cavalier(noir, new Coordonnee(1,0));
-            //tours
-        echiquier[0][7] =  new Tour	(noir, new Coordonnee(0,0));
-        echiquier[7][7] =  new Tour	(noir, new Coordonnee(7,0));
+        tableau[3][7] =  new Reine	(noir, new Coordonnee(3,0));
+        tableau[4][7] =  new Roi	(noir, new Coordonnee(4,0));
+        //fous
+        tableau[5][7] =  new Fou(noir, new Coordonnee(5,0));
+        tableau[2][7] =  new Fou(noir, new Coordonnee(2,0));
+        //cavaliers
+        tableau[6][7] =  new Cavalier(noir, new Coordonnee(6,0));
+        tableau[1][7] =  new Cavalier(noir, new Coordonnee(1,0));
+        //tours
+        tableau[0][7] =  new Tour	(noir, new Coordonnee(0,0));
+        tableau[7][7] =  new Tour	(noir, new Coordonnee(7,0));
 
         //PIONS
         for(int i=0; i<8; i++) {
-            echiquier[i][1] = new Pion(blanc, new Coordonnee(i, 1));
-            echiquier[i][6] = new Pion(noir, new Coordonnee(i, 6));
+            tableau[i][1] = new Pion(blanc, new Coordonnee(i, 1));
+            tableau[i][6] = new Pion(noir, new Coordonnee(i, 6));
         }
+
+        echiquier = new Echiquier(tableau);
+
+
+
+
     }
 
     public static Jeu instance() {
@@ -61,7 +67,7 @@ public class Jeu {
         return noir;
     }
 
-    public Piece[][] getEchiquier() {
+    public Echiquier getEchiquier() {
         return echiquier;
     }
 }
