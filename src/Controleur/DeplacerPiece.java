8a75e20ec6ca59bb800c7419bf467a4f15d68b67
Jean Pourroy <jean.pourroy@ensimag.grenoble-inp.fr>
2014-10-23 12:03:08 +0200

    Jean, debut controleur

--- src/Controleur/DeplacerPiece.java
+++ src/Controleur/DeplacerPiece.java
@@ -4,4 +4,8 @@ package Controleur;
  * Created by perrettc on 23/10/14.
  */
 public class DeplacerPiece {
+
+    //TODO verif DEPLACEMENT
+    //TODO verif ECHEK
+    //TODO sandBOX pour essayer les algo
 }
--- src/Controleur/Lancer_partie.java
+++ src/Controleur/Lancer_partie.java
@@ -1,13 +1,55 @@
 package Controleur;
 
+import Modèle.*;
+
 /**
  * Created by perrettc on 23/10/14.
  */
 public class Lancer_partie {
     public static void lancement_partie(){
-        //création des pièces pour les joueurs
 
-        //joueur blanc
+        //Création des 2 joueurs
+        Joueur blanc   = new Joueur();
+        Joueur noir    = new Joueur();
+
+
+
+        Piece[][] tableau = new Piece[8][8];
+        // BLANCS - Reine/Roi
+        tableau[3][0] =  new Reine(blanc, new Coordonnee(3,0));
+        tableau[4][0] =  new Roi(blanc, new Coordonnee(4,0));
+        //fous
+        tableau[5][0] =  new Fou(blanc, new Coordonnee(5,0));
+        tableau[2][0] =  new Fou(blanc, new Coordonnee(2,0));
+        //cavaliers
+        tableau[6][0] =  new Cavalier(blanc, new Coordonnee(6,0));
+        tableau[1][0] =  new Cavalier(blanc, new Coordonnee(1,0));
+        //tours
+        tableau[0][0] =  new Tour	(blanc, new Coordonnee(0,0));
+        tableau[7][0] =  new Tour	(blanc, new Coordonnee(7,0));
+
+        // NOIRS - Reine/Roi
+        tableau[3][7] =  new Reine	(noir, new Coordonnee(3,0));
+        tableau[4][7] =  new Roi	(noir, new Coordonnee(4,0));
+        //fous
+        tableau[5][7] =  new Fou(noir, new Coordonnee(5,0));
+        tableau[2][7] =  new Fou(noir, new Coordonnee(2,0));
+        //cavaliers
+        tableau[6][7] =  new Cavalier(noir, new Coordonnee(6,0));
+        tableau[1][7] =  new Cavalier(noir, new Coordonnee(1,0));
+        //tours
+        tableau[0][7] =  new Tour	(noir, new Coordonnee(0,0));
+        tableau[7][7] =  new Tour	(noir, new Coordonnee(7,0));
+
+        //PIONS
+        for(int i=0; i<8; i++) {
+            tableau[i][1] = new Pion(blanc, new Coordonnee(i, 1));
+            tableau[i][6] = new Pion(noir, new Coordonnee(i, 6));
+        }
+
+        echiquier = new Echiquier(tableau);
+
+
 
     }
 }
